ffaca8ee569ea919a14ca84b653d8eb7d5b4ff38
TANGEJ0411 <a123451121@gmail.com>
2022-12-03 15:33:36 +0800

    sum2

--- sum.js
+++ sum.js
@@ -5,3 +5,19 @@ function sum(n) {
 
 console.log(sum(3));
 console.log(sum(10));
+
+function sum1(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+console.log(sum1(3));
+console.log(sum1(10));
+
+function sum2(n) {
+
+}
+console.log(sum2(3));
+console.log(sum2(10));
