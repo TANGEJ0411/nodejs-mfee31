850f96993e9ba7d207bd6e0ecce22b1039abba24
TANGEJ0411 <a123451121@gmail.com>
2022-12-11 16:00:34 +0800

    var

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,11 +1,30 @@
 const axios=require('axios');
-
+const fs = require('fs');
+let readFilePromise = new Promise((resolve, reject) => {
+    fs.readFile('stock.txt', 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 
 try{
     (async ()=>{
-        let crawler = await axios.get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211');
+        let data = await readFilePromise;
+        let stockNo = data;
+        let date = '20221111';
+        let crawler = await axios.get(`http://54.71.133.152:3000/stocks`, {
+        params: {
+            'stockNo':stockNo,
+            date,
+        },
+        });
         console.log(crawler.data);
     })();
 }catch(err){
     console.log(err);
 }
+
+
