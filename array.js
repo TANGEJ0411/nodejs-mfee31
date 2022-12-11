4d134fe8dc72725a6e853c8737e305bcb9835060
TANGEJ0411 <a123451121@gmail.com>
2022-12-11 22:50:02 +0800

    prettier

--- array.js
+++ array.js
@@ -1,21 +1,21 @@
-ary1 = [-5, 1, 5, 3, 10, 17, 13, -2];
-ary2 = [100, 200, 300];
+let ary1 = [-5, 1, 5, 3, 10, 17, 13, -2];
+let ary2 = [100, 200, 300];
 
 //forEach
 
 function forEach(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i], i, array);
-    }
+	for (let i = 0; i < array.length; i++) {
+		callback(array[i], i, array);
+	}
 }
 forEach(ary1, function (item, index, array) {
-    console.log(item, index, array);
+	console.log(item, index, array);
 });
 
 //concat
 
 function concat(ary1, ary2) {
-    return [...ary1, ...ary2];
+	return [...ary1, ...ary2];
 }
 console.log(concat(ary1, ary2));
 //沒有影響到原陣列
@@ -23,9 +23,9 @@ console.log(concat(ary1, ary2));
 //push
 
 function push(ary, str) {
-    ary.length += 1;
-    ary[ary.length - 1] = str;
-    return ary;
+	ary.length += 1;
+	ary[ary.length - 1] = str;
+	return ary;
 }
 console.log(push(ary1, 300));
 //沒有改變原陣列
@@ -44,8 +44,8 @@ console.log(ary1);
 // }
 
 function pop(ary) {
-    ary.length -= 1
-    return ary
+	ary.length -= 1;
+	return ary;
 }
 console.log(pop(ary1));
 console.log(ary1);
