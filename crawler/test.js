e39d51d77efa2207b4ebe045fd93ef7da6d7732c
TANGEJ0411 <a123451121@gmail.com>
2022-12-17 11:54:25 +0800

    set.env

--- crawler/test.js
+++ crawler/test.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 (async()=>{
 	const connection =await mysql.createConnection({
-		host: '127.0.0.1',
-		port: 3306,
-		user: '',
-		password : '',
-		database : 'stock_mfee31',
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password : process.env.DB_PASSWORD,
+		database : process.env.DB_NAME,
 	});
-	let [result, fields] =await connection.query('SELECT * FROM `stocks`');
+	let [result, fields] =await connection.query('SELECT * FROM stocks');
 	console.log(result);
 	connection.end();
 })();
