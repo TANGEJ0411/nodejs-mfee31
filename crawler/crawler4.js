2e458a84836d2289f0ae360ca6b6e02567ead0bd
TANGEJ0411 <a123451121@gmail.com>
2022-12-17 14:54:35 +0800

    stock-back

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const fs = require('node:fs/promises');
-const mysql2 = require('mySql2');
 const moment =require('moment');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 /* let readFilePromise = new Promise((resolve, reject) => {
     fs.readFile('stock.txt', 'utf-8', (err, data) => {
@@ -13,8 +14,11 @@ const moment =require('moment');
     });
   }); */
 
-try {
-	(async () => {
+
+	
+(async () => {
+	let connection;
+	try {
 		let data = await fs.readFile('stock.txt', { encoding: 'utf8' });
 		let stockNo = data;
 		let date = '20221111';
@@ -25,9 +29,13 @@ try {
 			},
 		});
 		console.log(crawler.data);
-	})();
-} catch (err) {
-	console.log(err);
-}
+	} catch (err) {
+		console.error(err);
+	}finally{
+		if(connection){
+			connection.end();
+		}
+	}
+})();
 
 
